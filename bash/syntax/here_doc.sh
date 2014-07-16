4264eb335c6af367cef2040edf163f692fc4f723
consen <1186508815@qq.com>
2014-07-16 19:56:58 +0800

    add syntax fold.

---
+++ bash/syntax/here_doc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# we have less than 3 arguments. Print the help text:
+if [ $# -lt 3 ]; then
+    cat << HELP
+
+ren -- renames a number of files using sed regular expressions
+USAGE: ren 'regexp' 'replacement' files...
+EXAMPLE: rename all *.HTM files in *.html:
+    ren 'HTM$' 'html' *.HTM
+
+HELP
+    exit 0
+fi
