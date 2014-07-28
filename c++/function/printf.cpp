6bf6e3466c84a284bcb584edade93ff3c9605669
consen <1186508815@qq.com>
2014-07-28 11:18:30 +0800

    demo printf()'s return value.

--- c++/function/printf.cpp
+++ c++/function/printf.cpp
@@ -2,8 +2,13 @@
 
 int main()
 {
+    // first line, 1,            second line, 2
     printf("first line, %d,\
             second line, %d\n", 1, 2);
+
+    // printf return the number of characters printed
+    int i=43;
+    printf("%d\n",printf("%d",printf("%d",i)));     // 4321
 
     return 0;
 }
