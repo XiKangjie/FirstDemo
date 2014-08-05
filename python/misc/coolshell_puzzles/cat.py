2b5d2280d9121b1ec0d2213fbd8885d814c8afb1
consen <1186508815@qq.com>
2014-08-05 23:21:18 +0800

    fuck my brain.

--- python/misc/coolshell_puzzles/cat.py
+++ python/misc/coolshell_puzzles/cat.py
@@ -1,5 +1,17 @@
 import re
 string = open('cat.txt').read()
 #print(string)
-ans = re.search(r'([X-Z] | [0-9])([X-Z] | [0-9])[cat]\2\1', string)
-print(ans.group())
+it = re.finditer(r'(\w)(\w).\2\1', string)
+match = []
+for i in it:
+    # print(i.group())
+    match.append(i.group())
+
+#print(match)
+length = len(match)
+for i in range(0, length):
+    for j in range(i + 1, length):
+        for k in range(j + 1, length):
+            #if (match[i][1] + match[j][0] == match[k][1]) or (ord(match[j][1]) - ord(match[i][0]) == ord(match[k][0]) - ord(match[j][1])):
+            #if ord(match[j][1]) - ord(match[i][0]) == ord(match[k][0]) - ord(match[j][1]):
+                print(match[0], match[1], match[2])
