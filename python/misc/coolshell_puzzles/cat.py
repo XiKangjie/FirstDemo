ac9aef419cd9c7bbca83a85f2977b5f98f0a29e5
consen <1186508815@qq.com>
2014-08-06 13:15:29 +0800

    kill the cat, game over.

--- python/misc/coolshell_puzzles/cat.py
+++ python/misc/coolshell_puzzles/cat.py
@@ -1,3 +1,4 @@
+'''
 import re
 string = open('cat.txt').read()
 #print(string)
@@ -15,3 +16,13 @@ for i in range(0, length):
             #if (match[i][1] + match[j][0] == match[k][1]) or (ord(match[j][1]) - ord(match[i][0]) == ord(match[k][0]) - ord(match[j][1])):
             #if ord(match[j][1]) - ord(match[i][0]) == ord(match[k][0]) - ord(match[j][1]):
                 print(match[0], match[1], match[2])
+'''
+import re
+text = open('cat.txt').read()
+it = re.finditer(r'([A-Z])([0-9])[a-z]\2\1|([0-9])([A-Z])[a-z]\4\3', text)
+ans = ''
+for i in it:
+    print(i.group())
+    ans += i.group()[2]
+
+print('answer is : ', ans)
