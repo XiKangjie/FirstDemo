f0336ddffb94161e94f5dc3092242ef9641fd535
consen <1186508815@qq.com>
2014-08-13 17:07:17 +0800

    use requests.

--- python/misc/coolshell_puzzles/5_var.py
+++ python/misc/coolshell_puzzles/5_var.py
@@ -1,7 +1,16 @@
-import urllib.request
+# import urllib.request
 url = 'http://fun.coolshell.cn/n/'
-data = urllib.request.urlopen(url + '2014').read().decode('utf-8')
-while data.isdigit():
-    print(data)
-    data = urllib.request.urlopen(url + str(data)).read().decode('utf-8')
-print(data)     # Cool! the next level is "tree"
+# data = urllib.request.urlopen(url + '2014').read().decode('utf-8')
+# while data.isdigit():
+#     #print(data)
+#     data = urllib.request.urlopen(url + str(data)).read().decode('utf-8')
+# print(data)     # Cool! the next level is "tree"
+
+
+import requests
+num = '2014'
+while True:
+	num = requests.get(url + num).text
+	print(num)
+	if not num.isdigit():
+		break
