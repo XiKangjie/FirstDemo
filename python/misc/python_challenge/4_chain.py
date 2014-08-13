5125bee7a88b2f92f16876bcadf46ff3d58797ec
consen <1186508815@qq.com>
2014-08-13 13:35:25 +0800

    for 4th riddle.

--- python/misc/python_challenge/4_chain.py
+++ python/misc/python_challenge/4_chain.py
@@ -1,7 +1,8 @@
 import urllib.request
 url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
-data = urllib.request.urlopen(url + '12345').read().decode('utf-8')
-while data.isdigit():
+data = urllib.request.urlopen(url + '37278').read().decode('utf-8')
+while data.startswith('and'):
     print(data)
-    data = urllib.request.urlopen(url + str(data)).read().decode('utf-8')
-print(data) 
+    data = urllib.request.urlopen(url + data.split(' ')[-1]).read().decode('utf-8')
+print(data)
+# peak.html
