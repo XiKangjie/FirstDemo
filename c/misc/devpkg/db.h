031b6c9306a196ee5e396dc462b678bf8a2bded3
consen <1186508815@qq.com>
2014-09-30 14:29:40 +0800

    lcthw - a first real program, devpkg.

---
+++ c/misc/devpkg/db.h
@@ -0,0 +1,17 @@
+/*
+ * record URLs that heve been installed, list these URLs, and check if something
+ * has already been installed so that we can skip it.
+ */
+
+#ifndef _db_h
+#define _db_h
+
+#define DB_FILE "/usr/local/.devpkg/db"
+#define DB_DIR "/usr/local/.devpkg"
+
+int DB_init();
+int DB_list();
+int DB_update(const char* url);
+int DB_find(const char* url);
+
+#endif
