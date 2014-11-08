4875d4d48a44bd7ddef843ac4688dabce8f13096
consen <1186508815@qq.com>
2014-11-08 19:05:35 +0800

    var in bash is like macro in c.

---
+++ bash/syntax/var_macro.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo hello world
+echo hello        world
+echo "hello        world"
+
+c="hello        world"
+echo $c
+echo "$c"
+
+#hello world
+#hello world
+#hello        world
+#hello world
+#hello        world
