6ce8fe267937502e21455c6d0f0be30f16e372fe
consen <1186508815@qq.com>
2014-12-17 23:52:46 +0800

    python read binary file.

--- c/library/zlib/dec.py
+++ c/library/zlib/dec.py
@@ -1,12 +1,10 @@
 import zlib
 import struct
 
-with open("zlib_simple.z") as f:
-    print f.read(4)
-    #size = int(f.read(4))
-    #print size
-    #data = f.read(size)
-    #dedata = zlib.decompress(data)
-    #print len(dedata)
-    #s = struct.Struct('I4sI8s')
-    #print s.unpack(dedata)
+with open("zlib_simple.z", "rb") as f:
+    bsize = f.read(4)
+    size, = struct.unpack('I', bsize)
+    print size
+    data = f.read(size)
+    dedata = zlib.decompress(data)
+    print struct.unpack('I4sI8s', dedata)
