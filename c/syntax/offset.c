4bc8521161b5caa7e7826e584e9530a2547aed59
consen <1186508815@qq.com>
2015-01-06 19:20:47 +0800

    linux c offset.

---
+++ c/syntax/offset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct _Node {
+    int a;
+    long long b;
+    char* c;
+} Node;
+
+typedef struct _Node2 {
+    int a;
+    long long b;
+    char* c;
+} __attribute__((packed)) Node2;
+
+int main()
+{
+    printf("c offset: %lu\n", (unsigned long)(&( ((Node*)0)->c) ));     // 16
+    printf("c offset: %lu\n", (unsigned long)(&( ((Node2*)0)->c) ));    // 12
+
+    return 0;
+}
