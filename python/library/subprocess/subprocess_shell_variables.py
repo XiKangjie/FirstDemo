568de1eb1fc82621616d38ca7d33cc6742527621
consen <1186508815@qq.com>
2015-01-16 23:24:18 +0800

    python module subprocess.

--- python/library/subprocess/subprocess_shell_variables.py
+++ python/library/subprocess/subprocess_shell_variables.py
@@ -9,3 +9,10 @@ subprocess.call(['ls', '-l'], shell=True)
 # special shell features in the command string are processed before the
 # command is run.
 subprocess.call('echo $HOME', shell=True)
+
+
+# The return value from call() is the exit code of the program. The caller is
+# responsible for interpreting it to detect errors. The check_call() function
+# works like call() except that the exit code is checked, and if it indicates
+# an error happened then a CalledProcessError exception is raised.
+subprocess.check_call(['false'], shell=True)
