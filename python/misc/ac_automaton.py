1d361850498a163ee320271c8e33663cb33542b3
consen <1186508815@qq.com>
2015-03-24 00:14:49 +0800

    refine ac_automaton code.

--- python/misc/ac_automaton.py
+++ python/misc/ac_automaton.py
@@ -6,28 +6,26 @@ Aho–Corasick string matching algorithm
 
 Reference: http://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_string_matching_algorithm
 """
+from collections import defaultdict
 
 class ACState():
     def __init__(self, char=None):
-        self.goto = {}            # {char: ACState, ...}
-        self.failure = None
-        self.output = None
-        self.keyword_index = -1   # keyword index in keyword table
+        self.goto          = {}            # {char: ACState, ...}
+        self.failure       = None
+        self.output        = None
+        self.keyword_index = -1            # keyword index in keyword table
 
 
 class ACAutomaton():
     def __init__(self):
         self.root = ACState()
-        self.keyword_table = []
 
-    def add_keyword(self, keyword):
-        """
-        add keyword to keyword_table
-        """
-        self.keyword_table.append(keyword)
 
-    def build_goto(self):
-        for index, keyword in enumerate(self.keyword_table):
+    def build_goto(self, keyword_table):
+        """
+        build goto table, that's trie tree.
+        """
+        for index, keyword in enumerate(keyword_table):
             state = self.root
             for c in keyword:
                 if c not in state.goto:
@@ -37,7 +35,7 @@ class ACAutomaton():
 
     def build_failure_output(self):
         """
-        build failure table
+        build failure and output table
         """
         queue = []
         queue.append(self.root)
@@ -50,44 +48,76 @@ class ACAutomaton():
                 child.failure = failure.goto[char] if failure else self.root
                 
                 output = child.failure
-                while output:
-                    if output.keyword_index != -1:
-                        child.output = output
-                        break
+                while output and output.keyword_index == -1:
                     output = output.failure
+                child.output = output
 
                 queue.append(child)
 
-    def build(self):
-        self.build_goto()
+    def build(self, keyword_table):
+        self.build_goto(keyword_table)
         self.build_failure_output()
 
     def search(self, text):
-        for i, c in enumerate(text):
-            print 
+        """
+        search keywords in text
+        return a match dict,
+        key = index of char in text,
+        value = a list of index of matched keywords in keyword_table
+        """
+        match = defaultdict(list)
         state = self.root
         for i, c in enumerate(text):
             while state and c not in state.goto:
                 state = state.failure
             state = state.goto[c] if state else self.root
             if state.keyword_index != -1:
-                print self.keyword_table[state.keyword_index]
+                match[i].append(state.keyword_index)
             output = state.output
             while output:
-                print self.keyword_table[output.keyword_index]
+                match[i].append(output.keyword_index)
                 output = output.output
+        return match
 
 
 if __name__ == '__main__':
+    keyword_table = ['a', 'ab', 'bab', 'bc', 'bca', 'c', 'caa']
+    text = 'abccab'
+    
+    print 'keywords table:'
+    for i, k in enumerate(keyword_table):
+        print i, k
+    print '\ntext: ', text
+    
     ac = ACAutomaton()
-    for keyword in ['a', 'ab', 'bab', 'bc', 'bca', 'c', 'caa']:
-        ac.add_keyword(keyword)
-    ac.build()
-    ac.search('abccab')
+    ac.build(keyword_table)
+    match = ac.search(text)
+    
+    print '\nmatched:'
+    for i in match:
+        print i,
+        for index in match[i]:
+            print keyword_table[index],
+        print
 
-    print
-    ac = ACAutomaton()
-    for keyword in ['bca', 'ca', 'a']:
-        ac.add_keyword(keyword)
-    ac.build()
-    ac.search('bca')
+
+"""
+keywords table:
+0 a
+1 ab
+2 bab
+3 bc
+4 bca
+5 c
+6 caa
+
+text:  abccab
+
+matched:
+0 a
+1 ab
+2 bc c
+3 c
+4 a
+5 ab
+"""
