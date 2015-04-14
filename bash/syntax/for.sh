99df4edaf285a511e0155b66ee95900cb4814163
consen <1186508815@qq.com>
2015-04-14 10:52:17 +0800

    Use bash builtins for generating sequences

--- bash/syntax/for.sh
+++ bash/syntax/for.sh
@@ -1,6 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 A="I'm a"
 for var in A "A" 'A' "$A"; do
     echo "var is "$var""
+done
+#var is A
+#var is A
+#var is A
+#var is I'm a
+
+
+for i in {0..10..2}; do
+    echo $i
+done
+#0
+#2
+#4
+#6
+#8
+#10
+
+
+for ((i=0; i<5; i++)); do
+    echo $i
+done
+#0
+#1
+#2
+#3
+#4
+
+
+#list files
+for f in *; do
+    echo $f
 done
