f0ec571e4ce15078debcc22f6150a2e9abe3e5ab
consen <1186508815@qq.com>
2015-08-17 12:53:43 +0000

    python module SocketServer

---
+++ python/library/SocketServer/SocketServer_thread.py
@@ -0,0 +1,48 @@
+import threading
+import SocketServer
+
+class ThreadedEchoRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        # Echo the back to the client
+        data = self.request.recv(1024)
+        cur_thread = threading.currentThread()
+        response = '%s: %s' % (cur_thread.getName(), data)
+        self.request.send(response)
+        return
+
+# Adding threading or forking support to a server is as simple as including the appropriate mix-in
+# in the class hierarchy for the server. The mix-in classes override process_request() to start a
+# new thread or process when a request is ready to be handled, and the work is done in the new child.
+class ThreadedEchoServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+if __name__ == '__main__':
+    import socket
+    import threading
+
+    address = ('localhost', 0) # let the kernel give us a port
+    server = ThreadedEchoServer(address, ThreadedEchoRequestHandler)
+    ip, port = server.server_address # find out what port we were given
+
+    t = threading.Thread(target=server.serve_forever)
+    t.setDaemon(True) # don't hang on exit
+    t.start()
+    print 'Server loop running in thread:', t.getName()
+
+    # Connect to the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+
+    # Send the data
+    message = 'Hello, world'
+    print 'Sending : "%s"' % message
+    len_sent = s.send(message)
+
+    # Receive a response
+    response = s.recv(1024)
+    print 'Received: "%s"' % response
+
+    # Clean up
+    s.close()
+    server.socket.close()
