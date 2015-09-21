15596a519738bcd15b5e332df6a0849d39dad15b
consen <1186508815@qq.com>
2015-09-21 12:13:03 +0000

    multiprocessing.pool ThreadPool

--- python/library/multiprocessing/multiprocessing_pool.py
+++ python/library/multiprocessing/multiprocessing_pool.py
@@ -10,23 +10,31 @@ pool = None
 
 def f(x):
     logger.info('processing x: %s' % x) 
-    time.sleep(random.randint(1, 10))
-    return x * x
+    t = random.randint(1, 10)
+    time.sleep(t)
+    return x * x, t
 
 def print_result(result):
     print result
 
 def main():
     global pool
+    # maxtasksperchild is the number of tasks a worker process can complete before
+    # it will exit and be replaced with a fresh worker process, to enable unused
+    # resources to be freed. The default maxtasksperchild is None, 
+    # which means worker processes will live as long as the pool.
     pool = multiprocessing.Pool(processes=4, maxtasksperchild=2)
 
     results = []
-    for i in range(1, 10):
+    for i in range(1, 100):
         result = pool.apply_async(f, args=(i,), callback=print_result)
         results.append(result)
+
+    # some processes may have finished before result.get()
     for result in results:
         try:
-            result.get(timeout=3)
+            r, t = result.get(timeout=2)
+            print 'r: %s, t: %s' % (r, t)
         except multiprocessing.TimeoutError:
             print 'timeout'
 
