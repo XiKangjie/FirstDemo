3e4918ce1024b57d5aac56a39d6f6a100341bc16
consen <1186508815@qq.com>
2016-02-18 15:28:59 +0800

    python module platform

---
+++ python/library/platform/platform_python.py
@@ -0,0 +1,13 @@
+import platform
+
+print 'Version      :', platform.python_version()
+print 'Version tuple:', platform.python_version_tuple()
+print 'Compiler     :', platform.python_compiler()
+print 'Build        :', platform.python_build()
+
+"""
+Version      : 2.7.10
+Version tuple: ('2', '7', '10')
+Compiler     : GCC 4.4.7 20120313 (Red Hat 4.4.7-11)
+Build        : ('default', 'Oct 14 2015 09:01:55')
+"""
