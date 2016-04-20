23f4697d306d63e6311ec3df9c2a56be3d05f993
consen <1186508815@qq.com>
2016-04-20 10:56:20 +0800

    update python module click demo hello.py

--- python/library/click/hello.py
+++ python/library/click/hello.py
@@ -1,13 +1,27 @@
 import click
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
+@click.option('-c', '--count', default=1, help='Number of greetings.')
+@click.option('-n', '--first-second', prompt='Your name first second',
               help='The person to greet.')
-def hello(count, name):
+def hello(count, first_second):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
-        click.echo('Hello %s!' % name)
+        click.echo('Hello %s!' % first_second)
 
 if __name__ == '__main__':
     hello()
+
+"""
+$ python hello.py -n "aa bb"
+Hello aa bb!
+$ python hello.py --first-second "aa bb"
+Hello aa bb!
+$ python hello.py --first-second="aa bb"
+Hello aa bb!
+$ python hello.py -n="aa bb"
+Hello =aa bb!
+$ python hello.py -c 2 -n"aa bb"
+Hello aa bb!
+Hello aa bb!
+"""
