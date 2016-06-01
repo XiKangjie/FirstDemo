4955ff7c6b6949f7f669e9a7ef7207c90ab2fcc8
consen <1186508815@qq.com>
2016-06-01 15:09:18 +0800

    python module humanfriendly file size.

---
+++ python/library/humanfriendly/file_size.py
@@ -0,0 +1,15 @@
+from humanfriendly import format_size
+
+print format_size(100)
+print format_size(1024)
+print format_size(1024 * 1024)
+print format_size(1024 * 1024 * 1024)
+print format_size(10241892347)
+
+"""
+100 bytes
+1 KB
+1 MB
+1 GB
+9.54 GB
+"""
