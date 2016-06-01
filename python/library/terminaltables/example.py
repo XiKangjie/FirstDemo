d5083d4f41a45c3e24f6e9ffb398d666c0aa2c55
consen <1186508815@qq.com>
2016-06-01 10:50:12 +0800

    python module terminaltables

---
+++ python/library/terminaltables/example.py
@@ -0,0 +1,73 @@
+from terminaltables import AsciiTable, SingleTable, DoubleTable, GithubFlavoredMarkdownTable
+
+table_data = [
+    ["Name", "Size"],
+    ["test1", "512M"],
+    ["test2", "1G"],
+]
+
+ascii_table = AsciiTable(table_data)
+print ascii_table.table
+
+ascii_table.inner_row_border = True
+print ascii_table.table
+
+print
+
+single_table = SingleTable(table_data)
+print single_table.table
+
+single_table.inner_row_border = True
+print single_table.table
+
+single_table.title="List"
+print single_table.table
+
+print 
+
+gfw_table = GithubFlavoredMarkdownTable(table_data)
+print gfw_table.table
+
+
+"""
++-------+------+
+| Name  | Size |
++-------+------+
+| test1 | 512M |
+| test2 | 1G   |
++-------+------+
++-------+------+
+| Name  | Size |
++-------+------+
+| test1 | 512M |
++-------+------+
+| test2 | 1G   |
++-------+------+
+
+┌───────┬──────┐
+│ Name  │ Size │
+├───────┼──────┤
+│ test1 │ 512M │
+│ test2 │ 1G   │
+└───────┴──────┘
+┌───────┬──────┐
+│ Name  │ Size │
+├───────┼──────┤
+│ test1 │ 512M │
+├───────┼──────┤
+│ test2 │ 1G   │
+└───────┴──────┘
+┌List───┬──────┐
+│ Name  │ Size │
+├───────┼──────┤
+│ test1 │ 512M │
+├───────┼──────┤
+│ test2 │ 1G   │
+└───────┴──────┘
+
+| Name  | Size |
+|-------|------|
+| test1 | 512M |
+| test2 | 1G   |
+
+"""
