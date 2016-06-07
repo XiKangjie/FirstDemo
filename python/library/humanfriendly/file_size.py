7491c7235c354949c9f1fe0055791aac58ab8d92
consen <1186508815@qq.com>
2016-06-07 12:14:06 +0800

    python module humanfriendly format_size and parse_size.

--- python/library/humanfriendly/file_size.py
+++ python/library/humanfriendly/file_size.py
@@ -1,4 +1,4 @@
-from humanfriendly import format_size
+from humanfriendly import format_size, parse_size
 
 print format_size(100)
 print format_size(1024)
@@ -12,4 +12,16 @@ print format_size(10241892347)
 1 MB
 1 GB
 9.54 GB
+"""
+
+print parse_size('100 bytes')
+print parse_size('1 KB')
+print parse_size('1kb')
+print parse_size('1 MB')
+
+"""
+100
+1024
+1024
+1048576
 """
