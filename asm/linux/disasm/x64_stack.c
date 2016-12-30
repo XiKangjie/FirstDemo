c3170e6f5148cdeadcae4fe4d93b371508201fe9
consen <1186508815@qq.com>
2016-12-30 15:10:35 +0800

    x64 stack.

---
+++ asm/linux/disasm/x64_stack.c
@@ -0,0 +1,23 @@
+long utilfunc(long a, long b, long c)
+{
+    long xx = a + 2;
+    long yy = b + 3;
+    long zz = c + 4;
+    long sum = xx + yy + zz;
+
+    return xx * yy * zz + sum;
+}
+
+long myfunc(long a, long b, long c, long d,
+            long e, long f, long g, long h)
+{
+    long xx = a * b * c * d * e * f * g * h;
+    long yy = a + b + c + d + e + f + g + h;
+    long zz = utilfunc(xx, yy, xx % yy);
+    return zz + 20;
+}
+
+int main()
+{
+    myfunc(1, 2, 3, 4, 5, 6, 7, 8);
+}
