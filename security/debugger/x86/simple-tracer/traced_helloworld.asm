8d963c1e847297c85113b1a86629ea4f1e67ec17
consen <1186508815@qq.com>
2017-03-29 21:10:12 +0800

    [security] how debuggers work.

---
+++ security/debugger/x86/simple-tracer/traced_helloworld.asm
@@ -0,0 +1,28 @@
+; nasm -f elf32 -o traced_helloworld.o traced_helloworld.asm
+; ld -o traced_helloworld traced_helloworld.o
+
+section    .text
+    ; The _start symbol must be declared for the linker (ld)
+    global _start
+
+_start:
+    ; Prepare arguments for the sys_write system call:
+    ;   - eax: system call number (sys_write)
+    ;   - ebx: file descriptor (stdout)
+    ;   - ecx: pointer to string
+    ;   - edx: string length
+    mov    edx, len
+    mov    ecx, msg
+    mov    ebx, 1
+    mov    eax, 4
+
+    ; Execute the sys_write system call
+    int    0x80
+
+    ; Execute sys_exit
+    mov    eax, 1
+    int    0x80
+
+section   .data
+	msg db    'Hello, world!', 0xa
+	len equ    $ - msg
