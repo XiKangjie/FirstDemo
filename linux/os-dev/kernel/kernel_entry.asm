70c2dc6990a2b1d26a0df5d79ae3fd46a3be3ec8
consen <1186508815@qq.com>
2017-07-25 19:00:06 +0000

    [linux] os-dev, fix ld warning: cannot find entry symbol _start

--- linux/os-dev/kernel/kernel_entry.asm
+++ linux/os-dev/kernel/kernel_entry.asm
@@ -1,6 +1,10 @@
 ; Ensures that we jump straight into the kernel's entry function.
+global _start		; Fix ld warning: cannot find entry symbol _start
+
 [bits 32]			; We're in protected mode by now, so use 32-bit instructions.
-[extern main]		; Declate that we will be referencing the external symbol 'main',
-					; so the linker can substitute the final address
-call main			; invoke main () in our C kernel
-jmp $				; Hang forever when we return from the kernel
+
+_start:
+	[extern kernel_main]		; Declate that we will be referencing the external symbol 'kernel_main',
+								; so the linker can substitute the final address
+	call kernel_main			; invoke kernel_main() in our C kernel
+	jmp $						; Hang forever when we return from the kernel
